80248a42e744a4b60c5656e6aff578b36faee2fb
Dangggchris <christophermtdang@gmail.com>
2019-07-06 13:09:22 -0700

    added budget validations

--- models/budget.js
+++ models/budget.js
@@ -1,11 +1,33 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
   const budget = sequelize.define('budget', {
-    departmentName: DataTypes.STRING,
-    budgetTotal: DataTypes.INTEGER,
-    dateStart: DataTypes.DATE,
-    dateExpired: DataTypes.DATE,
-    username: DataTypes.STRING
+    departmentName: {
+      type: DataTypes.STRING,
+      allowNull:false,
+      validate: {
+        len: [1]
+      }
+    },
+    budgetTotal: {
+      type:DataTypes.DataTypes.DECIMAL(30, 2),
+      allowNull:false
+    },
+    dateStart: {
+      type:DataTypes.DATE,
+      allowNull:false
+    },
+    dateExpired: {
+      type:DataTypes.DATE,
+      allowNull:false
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull:false,
+      validate: {
+        len: [1]
+      }
+    }
   }, {});
   budget.associate = function(models) {
     // associations can be defined here
