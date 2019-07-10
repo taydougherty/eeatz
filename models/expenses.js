aa1ff7453bfcaf0786a0d024d00f88ca1b28a7e0
Dangggchris <christophermtdang@gmail.com>
2019-07-10 02:18:57 -0700

    updated seed files and sequel model

--- models/expenses.js
+++ models/expenses.js
@@ -1,6 +1,20 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const expenses = sequelize.define('expenses', {
+    restaurantId: {
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      validate: {
+        len: [1]
+      }
+    },
+    restaurantName: {
+      type:DataTypes.STRING,
+      allowNull:false,
+      validate: {
+        len: [1]
+      }
+    },
     departmentName: {
       type:DataTypes.STRING,
       allowNull:false,
