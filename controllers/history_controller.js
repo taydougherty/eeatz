1a0b8e8f4ba9c96dae388cadf836e0e475cba184
SMLM <farsight@hotmail.com>
2019-07-11 19:56:44 -0700

    mostly working edit budget

--- controllers/history_controller.js
+++ controllers/history_controller.js
@@ -18,3 +18,41 @@ exports.budget_history = function (req, res) {
         }
     })
 }
+exports.budgetDropdown = function (req, res) {
+    db.budgets.findAll({
+        attributes: ['departmentName'],
+        group: ['departmentName']
+    }).then(function (data) {
+        if (data.length > 0) {
+            res.json({
+                status: 200,
+                data: data
+            });
+        } else {
+            res.json({
+                status: 201,
+                msg: "Cannot find any data"
+            });
+        }
+    })
+}
+exports.budgetEditEntry = function (req, res) {
+    db.budgets.update(
+        // Values to update
+        {
+            departmentName: req.body.departmentName,
+            budgetTotal: req.body.budgetTotal,
+            dateStart: req.body.dateStart,
+            dateExpired: req.body.dateExpired
+        },
+        { // Clause
+            where:
+            {
+                id: req.body.id
+            }
+        }
+    ).then(function () {
+        console.log("asdf")
+        location.reload(true)
+    })
+}
--- routes/history.js
+++ routes/history.js
@@ -3,11 +3,12 @@ var router = express.Router();
 
 var history_controller = require('../controllers/history_controller');
 var isAuthenticated = require("../config/middleware/isAuthenticated");
-// I changed this to budget_history as the first table seen by default is the budget one
-router.get('/',isAuthenticated , history_controller.index);
+
+router.get('/', isAuthenticated, history_controller.index);
 
 router.get('/budgets', history_controller.budget_history)
-
+router.get('/budgetDropdown', history_controller.budgetDropdown)
+router.put('/budgets/editEntry', history_controller.budgetEditEntry)
 // router.get('/api/budget/filter:filter',history_controller.budget_history)
 // router.get('/expenses', history_controller.expenses_history);
 
